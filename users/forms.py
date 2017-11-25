e58d12ae40dbd1e0cfe1506e1e0579c35ce7ae4c
David Morel <david.morel@amakuru.net>
2017-11-25 23:04:58 +0100

    Better form widget handling (HTML5) and error display

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from address.forms import AddressField
+from datetime import date
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+import django.forms.widgets as widgets
+
 
 from .models import CustomUser
 
@@ -40,3 +43,15 @@ class UserForm(ModelForm):
         model = CustomUser
         fields = ['email', 'first_name', 'last_name', 'phone_number',
                   'street_address', 'birth_date', 'avatar_img', 'bio']
+        widgets = {
+            'birth_date': widgets.SelectDateWidget(years=range(1900, date.today().year + 1)),
+
+            # TODO: possible to switch to a jquery datepicker once the localization
+            # issue is fixed (see the user profile form, and base.html
+            #
+            # 'birth_date': forms.DateInput(attrs={'class':'datepicker'}),
+
+            'email': widgets.EmailInput(attrs={'type':'email'}),
+            'phone_number': widgets.TextInput(attrs={'type':'tel', 'placeholder': '+336012345678'}),
+        }
+        localized_fields = ['birth_date']

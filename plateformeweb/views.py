a9c2d7a01bdb89ca68fc1eb4e7d101491408e85a
David Morel <david.morel@amakuru.net>
2017-12-08 21:17:40 +0100

    Working version of Organization and OrganizationPeople

--- plateformeweb/views.py
+++ plateformeweb/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, FormView, CreateView, \
+    UpdateView
+from .models import Organization
+from django.urls import reverse
+
 
 def homepage(request):
     return render(request, 'plateformeweb/home.html')
+
+
+class OrganizationView(DetailView):
+    model = Organization
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class OrganizationListView(ListView):
+    model = Organization
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+# --- edit ---
+
+class OrganizationFormView():
+    model = Organization
+    fields = ["name", "active"]
+
+    def get_success_url(self):
+        return reverse('organization_detail', args=(self.object.pk,))
+
+
+class OrganizationCreateView(OrganizationFormView, CreateView):
+    pass
+
+
+class OrganizationEditView(OrganizationFormView, UpdateView):
+    pass
+
+# ------
+
 

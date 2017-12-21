0763e7747a61749da585439c1a27f50f93aaf34d
David Morel <david.morel@amakuru.net>
2017-12-21 22:50:10 +0100

    Add events for user in context for all pages

--- ateliersoude/settings.py
+++ ateliersoude/settings.py
@@ -104,6 +104,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'plateformeweb.context_processors.user_data',
             ],
         },
     },
---
+++ plateformeweb/context_processors.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AnonymousUser
+from django.utils.timezone import now
+from .models import Event, PublishedEvent
+from users.models import CustomUser
+
+# NOTE requiert plateformeweb.context_processors.user_data dans les context
+# processors de settings.py
+
+# rajoute 2 variables de template dans le contexte
+# - my_events_attending (events futurs ou le user participe, publiés uniquement)
+# - my_events_organizing (tous les events futurs où le user organise, publiés
+#   ou non)
+
+# les variables de templates donnent accès au queryset (lazy evaluation, donc
+# pas de requête si pas d'affichage), exemple:
+
+# <h1>
+#     <ul>
+#         {% for item in my_events_attending %}
+#             <li>{{ item.title }}</li>
+#         {% endfor %}
+#     </ul>
+#     <ul>
+#         {% for item in my_events_organizing %}
+#             <li>{{ item.title }}</li>
+#         {% endfor %}
+#     </ul>
+# </h1>
+
+def user_data(request):
+    if isinstance(request.user, AnonymousUser) or request.user is None:
+        return {}
+    events_attending = PublishedEvent.objects.all().filter(
+        attendees=request.user).filter(
+        ends_at__gte=now())
+    events_organize = Event.objects.all().filter(
+        organizers=request.user).filter(
+        ends_at__gte=now())
+    return {'my_events_attending': events_attending,
+            'my_events_organizing': events_organize}

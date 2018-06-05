3e47d9e1f98ec47d69f12360a55a698f908bb20e
Bastien Versini <versini.bastien@protonmail.com>
2018-06-05 15:17:23 +0200

    Fix celery throwing various errors

--- ateliersoude/celery.py
+++ ateliersoude/celery.py
@@ -1,10 +1,38 @@
+from __future__ import absolute_import, unicode_literals
 from celery import Celery
-from celery import task
+from celery.schedules import crontab
+from django.core import management
 import os
+import django
+import tasks
 
-celery = Celery('ateliersoude', broker='redis://password@redis:6379/0') #!
-os.environ[ 'DJANGO_SETTINGS_MODULE' ] = "settings"
+# set the default Django settings module for the 'celery' program.
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ateliersoude.settings'
 
-@task()
-def add_photos_task( lad_id ):
-    print("HelLO WORkLD")
+app = Celery('ateliersoude')
+
+app.conf.broker_url = 'redis://redis:6379/0'
+app.conf.result_backend = 'redis://redis:6379/0'
+app.conf.accept_content = ['application/json']
+app.conf.result_serializer = 'json'
+app.conf.task_serializer = 'json'
+app.conf.timezone = 'Europe/Paris'
+app.conf.beat_schedule = {
+	# every minute
+	'every-minute': {
+		'task': 'tasks.send_queued_mail',
+		'schedule': crontab(),
+                'args': ()
+	}
+}
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+@app.task()
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+@app.task(name='tasks.send_queued_mail')
+def send_queued_mail():
+    management.call_command('send_queued_mail')

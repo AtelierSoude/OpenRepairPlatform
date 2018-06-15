5ddc99d3f74b195af3a63170fd21e26fc33bf6c0
vinny <jums@inventaire.io>
2018-06-15 15:52:31 +0200

    add basic model tests

---
+++ plateformeweb/tests/tests_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from plateformeweb.models import PlaceType
+
+
+
+class TestPlaceTypeModel(TestCase):
+    "place type tests"
+
+    def setUp(self):
+        PlaceType.objects.create(name="Atelier Soudé", slug="ateliersoude")
+
+    def test_place_type(self):
+        slug = "ateliersoude"
+        latelier = PlaceType.objects.get(slug=slug)
+        self.assertEqual(latelier.slug, slug)
+        self.assert(latelier.name)

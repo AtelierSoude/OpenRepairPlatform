f51c96e29f1a64aa255b0064b86eae5e86ca412d
Bastien Versini <versini.bastien@protonmail.com>
2018-07-25 16:11:08 +0200

    Context no longer needed, goodbye n+1 requests in Place List

--- plateformeweb/views.py
+++ plateformeweb/views.py
@@ -99,7 +99,7 @@ class PlaceListView(ListView):
     model = Place
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = {}
         context["list_type"] = "place"
         return context
 

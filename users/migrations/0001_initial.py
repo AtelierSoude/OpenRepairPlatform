7a3e77450f0dd7dd0bd4c40501d9d1ead8ca4916
Vincent Jumo <jums@inventaire.io>
2018-12-10 14:23:31 +0100

    replace phone number special field with basic charField

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -7,7 +7,6 @@ import avatar.models
 from django.db import migrations, models
 import django.db.models.deletion
 import django.utils.timezone
-import phonenumber_field.modelfields
 
 
 class Migration(migrations.Migration):
@@ -33,7 +32,7 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('phone_number', phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=128, null=True, verbose_name='phone number')),
+                ('phone_number', models.CharField(blank=True, max_length=15, verbose_name='phone number')),
                 ('birth_date', models.DateField(blank=True, null=True, verbose_name='date of birth')),
                 ('gender', models.CharField(choices=[('m', 'Male'), ('f', 'Female'), ('o', 'Other')], max_length=1, null=True)),
                 ('avatar_img', avatar.models.AvatarField()),

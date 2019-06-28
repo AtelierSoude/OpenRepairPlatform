0c294034279761f41d3855cd419db570434da1e4
Dynnammo <baptiste.thivend@insa-lyon.fr>
2019-06-28 17:04:36 +0200

    Add french translation

---
+++ ateliersoude/event/middleware/middleware.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.utils import translation
+from django.utils.deprecation import MiddlewareMixin
+
+
+class ForceLangMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        request.LANG = getattr(
+            settings,
+            'LANGUAGE_CODE',
+            settings.LANGUAGE_CODE
+        )
+        translation.activate(request.LANG)
+        request.LANGUAGE_CODE = request.LANG

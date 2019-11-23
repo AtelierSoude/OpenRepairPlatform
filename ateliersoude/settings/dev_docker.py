f0ceb83080ef0f21eaaa99a24fbcf9c0b532160b
clement <clement.vial@gmail.com>
2019-11-23 10:11:09 +0100

    make debug toolbar appear

--- ateliersoude/settings/dev_docker.py
+++ ateliersoude/settings/dev_docker.py
@@ -1,12 +1,16 @@
 from .base import *  # noqa
 import os
+import socket
 
 
-INSTALLED_APPS += ["debug_toolbar"]  # noqa
+DEBUG = True
 
-MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa
+INSTALLED_APPS += ['debug_toolbar', ]
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + ['127.0.0.1', '10.0.2.2']
 
-INTERNAL_IPS = ["127.0.0.1"]
+ALLOWED_HOSTS = ALLOWED_HOSTS + ['localhost', '127.0.0.1', 'django']
 
 DATABASES = {
     "default": {

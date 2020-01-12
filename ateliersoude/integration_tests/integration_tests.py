2f44532caabf5ab7ab8fb2f063be6f7b352aaea2
clement <clement.vial@gmail.com>
2020-01-12 18:48:51 +0100

    correct PEP

--- ateliersoude/integration_tests/integration_tests.py
+++ ateliersoude/integration_tests/integration_tests.py
@@ -14,6 +14,7 @@ import ptvsd
 # ptvsd.enable_attach()
 # ptvsd.wait_for_attach()
 
+
 class TestGeneral:
     def setup_class(self):
         self.driver = webdriver.Chrome()
@@ -21,7 +22,8 @@ class TestGeneral:
     def test_home(self):
         xpath = "/html/body/main/div/div[1]/section/div/div/form/div[6]/button"
         self.driver.get("django:8000")
-        element = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        element = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, xpath)))
         assert 1 == 1
 
     def test_admin_login(self):
@@ -29,7 +31,8 @@ class TestGeneral:
         # go to main page
         xpath = "/html/body/main/div/div[1]/section/div/div/form/div[6]/button"
         self.driver.get("django:8000")
-        element = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        element = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+            (By.XPATH, xpath)))
 
         # click on connexion
         xpath = "/html/body/header/nav/div[2]/div/small/a"
@@ -37,20 +40,25 @@ class TestGeneral:
         logbutton.click()
 
         # fill the id and connect
-        xpath_connect = "/html/body/main/section/div[2]/div[2]/div/form/div[4]/button"
-        connectbutton = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath_connect)))
+        xpath_connect = "/html/body/main/section"
+        "/div[2]/div[2]/div/form/div[4]/button"
+        connectbutton = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, xpath_connect)))
 
         xpath_email = "//*[@id='id_username']"
-        email = self.driver.find_elements(By.XPATH, xpath_email)[0].send_keys("admin@example.com")
+        email = self.driver.find_elements(
+            By.XPATH, xpath_email)[0].send_keys("admin@example.com")
 
         xpath_pwd = "//*[@id='id_password']"
-        pwd = self.driver.find_elements(By.XPATH, xpath_pwd)[0].send_keys("adminpass")
+        pwd = self.driver.find_elements(
+            By.XPATH, xpath_pwd)[0].send_keys("adminpass")
 
         connectbutton.click()
 
         # check that the login is successful
         xpath_meetings = "/html/body/main/div/div[2]/h4[2]/a"
-        meetingsbutton = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath_meetings)))
+        meetingsbutton = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, xpath_meetings)))
 
         assert meetingsbutton.tag_name == 'a'
 
@@ -59,23 +67,28 @@ class TestGeneral:
         # xpath for the validation button
         xpath_validation = "/html/body/main/div[2]/div/form/div[10]/input"
         # wait for the page to be loaded
-        validation_button = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath_validation)))
+        validation_button = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, xpath_validation)))
 
         # orga name
         xpath = "//*[@id='id_name']"
-        self.driver.find_elements(By.XPATH, xpath)[0].send_keys("orga " + str(datetime.now()))
+        self.driver.find_elements(By.XPATH, xpath)[0].send_keys(
+            "orga " + str(datetime.now()))
 
         xpath = "//*[@id='id_description_ifr']"
-        self.driver.execute_script("tinyMCE.activeEditor.setContent('%s')" % "dscription")
+        self.driver.execute_script(
+            "tinyMCE.activeEditor.setContent('%s')" % "dscription")
 
         xpath = "//*[@id='id_email']"
-        self.driver.find_elements(By.XPATH, xpath)[0].send_keys("orga@orga.com")
+        self.driver.find_elements(
+            By.XPATH, xpath)[0].send_keys("orga@orga.com")
 
         xpath = "//*[@id='id_phone_number']"
         self.driver.find_elements(By.XPATH, xpath)[0].send_keys("0666666666")
 
         xpath = "//*[@id='id_picture']"
-        self.driver.find_elements(By.XPATH, xpath)[0].send_keys("/tests/images/img01.jpg")
+        self.driver.find_elements(
+            By.XPATH, xpath)[0].send_keys("/tests/images/img01.jpg")
 
         xpath = "//*[@id='id_min_fee']"
         self.driver.find_elements(By.XPATH, xpath)[0].send_keys("16")
@@ -86,14 +99,11 @@ class TestGeneral:
         validation_button.click()
 
         xpath_success = "/html/body/div[1]/div"
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath_success)))
+        WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, xpath_success)))
 
         assert True
 
     def teardown_class(self):
         # delete the organization
-        
-
-
-
         self.driver.close()

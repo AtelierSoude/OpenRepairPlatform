ddf7c7d2ab4d59c78a88c912ecc8016bc464045e
Clément Poudret <clement.poudret@laposte.net>
2020-08-29 11:31:41 +0200

    improve inventory models

--- ateliersoude/inventory/admin.py
+++ ateliersoude/inventory/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 
-from .models import Stuff
+from .models import Stuff, Device, Brand, Category, SubCategory
 
 
+admin.site.register(Brand, SimpleHistoryAdmin)
+admin.site.register(Category, SimpleHistoryAdmin)
+admin.site.register(SubCategory, SimpleHistoryAdmin)
+admin.site.register(Device, SimpleHistoryAdmin)
 admin.site.register(Stuff, SimpleHistoryAdmin)
--- ateliersoude/inventory/models.py
+++ ateliersoude/inventory/models.py
@@ -4,6 +4,37 @@ from simple_history.models import HistoricalRecords
 from ateliersoude.fields import CleanHTMLField
 from ateliersoude.user.models import Organization
 
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(
+        upload_to=None,
+        height_field=None,
+        width_field=None,
+        max_length=None,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey(
+        "inventory.Category", 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        )
+    
+    def __str__(self):
+        return self.name
 
 class Stuff(models.Model):
     BROKEN = "B"
@@ -18,16 +49,23 @@ class Stuff(models.Model):
         (FIXING, "Fixing"),
         (THROWN, "Thrown"),
     ]
+    device = models.ForeignKey(
+        "inventory.device",
+        related_name="device",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
     member_owner = models.ForeignKey(
         "user.CustomUser",
-        related_name="stock",
+        related_name="member_owner",
         null=True,
         blank=True,
         on_delete=models.CASCADE
     )
     organization_owner = models.ForeignKey(
         "user.Organization",
-        related_name="stock",
+        related_name="organization_owner",
         null=True,
         blank=True,
         on_delete=models.CASCADE
@@ -41,10 +79,10 @@ class Stuff(models.Model):
     )
     place = models.ForeignKey(
         "location.Place",
-        related_name="stock",
+        related_name="place",
         null=True,
         blank=True,
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL
     )
     added_date = models.DateField(
         auto_now_add=True,
@@ -73,7 +111,15 @@ class Stuff(models.Model):
         return reverse(
             "inventory:stuff_view", kwargs={'stuff_pk': self.pk}
         )
+    
+    def __str__(self):
+        return f"{self.device} - #{self.id}"
 
+class Part(Stuff):
+    pass
+
+class Tool(Stuff):
+    pass
 
 class RepairFolder(models.Model):
     Stuff = models.ForeignKey("inventory.Stuff", on_delete=models.CASCADE)
@@ -97,34 +143,101 @@ class Repair(models.Model):
         return self.repair_date
 
 
-class Device(Stuff):
-    category = models.CharField(max_length=50)
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    description = CleanHTMLField()
+class Device(models.Model):
+    category = models.ForeignKey(
+        "inventory.Category", 
+        on_delete=models.SET_NULL,  
+        blank=True,
+        null=True,
+    )
+    subcategory = models.ForeignKey(
+        "inventory.SubCategory",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    brand = models.ForeignKey(
+        "inventory.Brand",  
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    model = models.CharField(
+        max_length=50, 
+        null=True,
+        blank=True
+    )
+    description = CleanHTMLField(
+        null=True,
+        blank=True
+    )
     picture = models.ImageField(
         upload_to=None,
         height_field=None,
         width_field=None,
-        max_length=None
+        max_length=None,
+        null=True,
+        blank=True
     )
-    defect = models.ForeignKey("inventory.Defect", on_delete=models.CASCADE)
+    defect = models.ForeignKey(
+        "inventory.Defect", 
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
-        return self.category
-
-
-class Part(Stuff):
-    pass
-
-
-class Tool(Stuff):
-    pass
+        return f"{self.subcategory}-{self.brand}-{self.model}"
 
 
 class Defect(models.Model):
     pass
 
+# NOTE: Comments below are present to help modelling the repair process and objects
+# and list fields that should be in the classes
 
+# Observation as done by the owner of the device (either initially or during repair or diagnosis)
+# Implement a string e.g. "No power on the output of the switch even when on"
+# or "After house was hit by a lightning, the dishwasher would not start anymore" as the initial observation
+# Maybe add a list of strings that would be hashtags for indexing e.g. "#NoPower", "#Switch", "#PowerSupply"
+# Maybe link to a user or fixer to indicate who made the Observation
+class Observation(models.Model):
+    pass
+    
+# A reasoning that takes place during repair or diagnosis
+# Implement a string that would detail fixer reasoning from an Observation and leading to an Action
+# e.g. "There is no power at the output pin of the switch even when the switch is on. The switch must be defective"
+# Maybe link to a user or fixer to indicate who made the Reasoning
+class Reasoning(models.Model):
+    pass
+
+# An action taken by the fixer during repair or diagnosis
+# Could be implemented as a verb (from a list that must be easily maintained) associated to a Part
+# e.g. Replace capacitor C3
+# or Resolder Wire Red wire
+# Maybe link to a user or fixer to indicate who took the Action
 class Action(models.Model):
+    pass
+    
+# Implements a status (be it the initial one or the one resulting from an Action being taken)
+# Take it from the Stuff class above
+class Status(models.Model):
+    pass
+
+# Implements a step in diagnosis or repair.
+# This consists of:
+# 1. An Observation
+# 2. An optional Reasoning (from the observation) leading to an optional Action
+# 3. An optional Action
+# 4. A resulting status after Action is taken
+# The initial (first) DiagnosisOrRepairStep instance would contain 
+# . the initial Observation and
+# . the intial status (probably "broken" unless the user comes for an enhancement or minor issue)
+class DiagnosisOrRepairStep(models.Model):
+    pass
+    
+# Implements a complete diagnosis or a complete repair
+# This would maybe replace or be merged within the Repair class above
+# Consists mainly of a sequence of DiagnosisOrRepairSteps as define before
+class DiagnosisOrRepair(models.Model):
     pass

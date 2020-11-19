cb6f6d1a08771731dd36690f73e0f9be69a2970a
lucien <lucien@hashbang.fr>
2020-11-19 14:44:40 +0100

    Add vue in project

---
+++ openrepairplatform/static/js/vue/components/Intervention.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Intervention',
+  components: {},
+  data () {
+    return {
+      title: 'Intervention'
+    }
+  },
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>

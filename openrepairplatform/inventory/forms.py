4bda7bbfc7755af831f0e80464f490efbd48c7b0
Clément Poudret <clement.poudret@laposte.net>
2020-12-03 14:52:19 +0100

    StuffForm is OK

--- openrepairplatform/inventory/forms.py
+++ openrepairplatform/inventory/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from django import forms
 from openrepairplatform.user.models import CustomUser
 from openrepairplatform.location.models import Place
-from .models import Stuff, Device, Category, Observation, RepairFolder, Intervention, Brand
+from .models import Stuff, Device, Category, Observation, RepairFolder, Intervention, Brand, Reasoning, Action, Status
 from dal import autocomplete, forward
 from bootstrap_modal_forms.forms import BSModalModelForm
 
@@ -47,7 +47,15 @@ class StuffForm(BSModalModelForm):
     category = forms.ModelChoiceField(
         widget=autocomplete.ModelSelect2(url='inventory:category_autocomplete'),
         label="Catégorie d'appareil",
+        required = False,
         queryset = Category.objects.all(),
+    )
+    device = forms.ModelChoiceField(
+        widget=autocomplete.ModelSelect2(url='inventory:device_autocomplete', 
+        forward=['category']),
+        label="Type d'appareil",
+        queryset = Device.objects.all(),
+        required = False
     )
     create_device = forms.BooleanField(
         label = "Je ne trouve pas mon type d'appareil dans la liste ci dessus",
@@ -74,67 +82,74 @@ class StuffForm(BSModalModelForm):
         required = False, 
     )
     ongoing = forms.BooleanField(
-        label = "Ce dossier est-il clos ?",
+        label = "Ce dossier est-il en cours ?",
         required = False, 
+        initial= True,
     )
-    observation = forms.ChoiceField(
-        widget=autocomplete.Select2(url='inventory:observation_autocomplete'),
+    observation = forms.ModelChoiceField(
+        widget=autocomplete.ModelSelect2(url='inventory:observation_autocomplete'),
         help_text="Quel est (ou était) le problème ?",
-        required = False
+        required = False,
+        queryset = Observation.objects.all()
     )
-    reasoning = forms.ChoiceField(
-        widget=autocomplete.Select2(url='inventory:reasoning_autocomplete'),
+    reasoning = forms.ModelChoiceField(
+        widget=autocomplete.ModelSelect2(url='inventory:reasoning_autocomplete'),
         help_text="Quel en est (ou serait) la cause ?",
         label="Raisonnement",
-        required = False
+        required = False,
+        queryset = Reasoning.objects.all()
     )
-    action = forms.ChoiceField(
-        widget=autocomplete.Select2(url='inventory:action_autocomplete'),
+    action = forms.ModelChoiceField(
+        widget=autocomplete.ModelSelect2(url='inventory:action_autocomplete'),
         help_text="Qu'avez-vous fait ?",
-        required = False
+        required = False,
+        queryset = Action.objects.all()
     )
-    status = forms.ChoiceField(
-        widget=autocomplete.Select2(url='inventory:status_autocomplete'),
+    status = forms.ModelChoiceField(
+        widget=autocomplete.ModelSelect2(url='inventory:status_autocomplete'),
         help_text="Quel est le résultat de l'action ?",
-        required = False
+        required = False,
+        queryset = Status.objects.all()
     )
-
-    def clean_create_folder(self):
-        self.folder = {}
-        self.intervention = {}
-        self.create_folder = self.cleaned_data.pop('create_folder')
-        if self.create_folder:
-            self.folder['repair_date'] = self.cleaned_data.pop('repair_date')
-            self.folder['ongoing'] = self.cleaned_data.pop('ongoing')
-            self.intervention['observation'] = self.cleaned_data.pop('observation')
-            self.intervention['reasoning'] = self.cleaned_data.pop('reasoning')
-            self.intervention['action'] = self.cleaned_data.pop('action')
-            self.intervention['status'] = self.cleaned_data.pop('status')
-            for key, value in self.folder:
-                if not value:
-                    self.add_error(key, 'Ce champ ne peut pas être vide.')
-            for key, value in self.intervention:
-                if not value:
-                    self.add_error(key, 'Ce champ ne peut pas être vide.')
 
     def clean_device(self):
         device = self.cleaned_data['device']
-        import pdb; pdb.set_trace()
         if not device:
             device = {}
-            device["category"] = self.cleaned_data.pop('category')
-            device["brand"] = self.cleaned_data.pop('brand')
-            device["model"] = self.cleaned_data.pop('model')
+            device["category"] = self.cleaned_data['category']
+            device["brand"] = self.cleaned_data['brand']
+            device["model"] = self.cleaned_data['model']
             if not device["category"]:
               self.add_error("category", "Ce champ ne peut pas être vide")
             device = Device.objects.create(**device)
-            self.cleaned_data['device'] = device.pk  
+            self.cleaned_data['device'] = device
+        return self.cleaned_data['device']
+
+    def init_folder(self, data):
+        self.folder = {}
+        self.intervention = {}
+        self.folder['open_date'] = data['repair_date']
+        self.folder['ongoing'] = data['ongoing']
+        self.intervention['repair_date'] = data['repair_date']
+        self.intervention['observation'] = data['observation']
+        self.intervention['reasoning'] = data['reasoning']
+        self.intervention['action'] = data['action']
+        self.intervention['status'] = data['status']
+        for key, value in self.folder.items():
+            if not value:
+                self.add_error(key, f'le champ {key} ne peut pas être vide.')
+        for key, value in self.intervention.items():
+            if not value:
+                self.add_error(key, f'le champ {key} ne peut pas être vide.')
+        self.create_folder = data['create_folder']
 
     def clean(self):
         if getattr(self, "user", False):
             self.cleaned_data["member_owner"] = self.user
         if getattr(self, "organization", False):
             self.cleaned_data["organization_owner"] = self.organization
+        if self.cleaned_data['create_folder']: 
+            self.init_folder(self.cleaned_data)
 
     def save(self, commit=True):
             instance = super().save(commit=commit)
@@ -146,23 +161,15 @@ class StuffForm(BSModalModelForm):
 
     def __init__(self, organization=None, user=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['device'] = forms.ModelChoiceField(
-                widget=autocomplete.ModelSelect2(url='inventory:device_autocomplete', 
-                forward=['category']),
-                label="Type d'appareil",
-                queryset = Device.objects.all(),
-                required = False
-            )
         if organization:
             self.organization = organization
-            self.Meta.fields += ('place',)
-            self.fields['place'] = forms.ChoiceField(
-                widget=autocomplete.Select2(url='place_autocomplete'),
+            self.fields['place'] = forms.ModelChoiceField(
+                widget=autocomplete.ModelSelect2(url='place_autocomplete'),
                 label="Localisation",
+                queryset= Place.objects.all(),
                 help_text="Où se trouve l'appareil ?",
                 required = False
             )
-            self.fields['place'].initial = 'whatever you want'
         elif user:
             self.user = user 
         else:
@@ -174,12 +181,6 @@ class StuffForm(BSModalModelForm):
             "category",
             "create_folder",
             "create_device",
-            "ongoing",
-            "observation",
-            "action",
-            "reasoning",
-            "status",
-            "repair_date",
             "brand",
             "model",
             "state",
@@ -187,6 +188,12 @@ class StuffForm(BSModalModelForm):
             "member_owner",
             "information",
             "device",
+            "ongoing",
+            "observation",
+            "action",
+            "reasoning",
+            "status",
+            "repair_date",
         )
 
 

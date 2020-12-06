0ced985e895d3fd4d4b3fd3c4bb74f7958864b75
Clément Poudret <clement.poudret@laposte.net>
2020-12-07 00:12:04 +0100

    try to make select2 works

--- openrepairplatform/static/js/inventory/create_stuff.js
+++ openrepairplatform/static/js/inventory/create_stuff.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $.fn.modal.Constructor.prototype._enforceFocus = function() {};
+  });
 
+$(document).ready(function(){
   category = document.querySelector("#id_category");
   if (category) {
     category.onchange=function() {  

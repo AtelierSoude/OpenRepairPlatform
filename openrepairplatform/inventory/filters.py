9df81769286f36ddbc65df38972269017f0f9211
Clément Poudret <clement.poudret@laposte.net>
2020-12-12 20:50:29 +0100

    add autocomplete on organization event search

--- openrepairplatform/inventory/filters.py
+++ openrepairplatform/inventory/filters.py
@@ -1,12 +1,16 @@
 import django_filters
 from django.db import models
 from django import forms
+from dal import autocomplete 
 
 from openrepairplatform.inventory.models import (
-    Stuff
+    Stuff,
+    Device,
+    Category
 )
+
 class StockFilter(django_filters.FilterSet):
-   
+
     class Meta:
         model = Stuff
         fields = ["device", "id", "state"]

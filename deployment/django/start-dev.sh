923f98b096e39b417a246475c27c671a99b6a4b7
Thomas Gudin <gudinthomas@gmail.com>
2022-12-21 18:00:11 +0100

    fix(compose): fix dev compose startup

---
+++ deployment/django/start-dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#sed -i "s/{{SECRET_KEY}}/${SECRET_KEY}/" /etc/cron.d/openrepairplatform
+#sed -i "s/{{EMAIL_PASSWORD}}/${EMAIL_PASSWORD}/" /etc/cron.d/openrepairplatform
+#service cron start
+#chown -R openrepairplatform:openrepairplatform /srv/*
+python3 ../../manage.py migrate
+python3 ../../manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
+python3 ../../manage.py runserver
+#uwsgi ./uwsgi.ini

c4e6c2713965dd5209c3909f62e85c604349e559
Thomas Gudin <gudinthomas@gmail.com>
2023-03-02 13:35:37 +0100

    feat(start): update start dev

--- deployment/django/start-dev.sh
+++ deployment/django/start-dev.sh
@@ -3,11 +3,11 @@
 #sed -i "s/{{SECRET_KEY}}/${SECRET_KEY}/" /etc/cron.d/openrepairplatform
 #sed -i "s/{{EMAIL_PASSWORD}}/${EMAIL_PASSWORD}/" /etc/cron.d/openrepairplatform
 #service cron start
-chown -R openrepairplatform:openrepairplatform /srv/*
+#chown -R openrepairplatform:openrepairplatform /srv/*
 python3 ../../manage.py migrate
 python3 ../../manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
 #python3 ../../manage.py collectstatic --noinput
 #uwsgi ./uwsgi.ini
 
-python3 ../../manage.py runserver 0.0.0.0:8000
+python3 ../../manage.py runserver 0.0.0.0:80 --insecure
 #uwsgi ./uwsgi.ini

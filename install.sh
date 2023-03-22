c33b8d2ae400c6405c847661b2311a99caaa7d31
Thomas Gudin <gudinthomas@gmail.com>
2023-03-22 16:02:58 +0100

    fix6

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ docker compose -f ./docker-compose-initiate.prod.yml down
  
 # some configurations for let's encrypt
 curl -L --create-dirs -o ./deployment/nginx/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
-openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
+openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
  
 # Phase 2
-crontab ./etc/crontab
-docker compose -f ./docker-compose.prod.yml -d up
+#crontab /etc/crontab
+docker compose -f ./docker-compose.prod.yml up -d

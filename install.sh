fbc581c45a31889309f0c471cfe8a440b7fe296d
Thomas Gudin <gudinthomas@gmail.com>
2023-03-22 16:44:15 +0100

    fix en v files command

--- install.sh
+++ install.sh
@@ -5,13 +5,12 @@ EMAIL=$2
 
 # TODO : generate .env variables
 #echo MARIADB_USER=matomo > .env.prod
-#echo MARIADB_PASSWORD=`openssl rand 30 | base64 -w 0` >> .env.prod
-#echo MARIADB_ROOT_PASSWORD=`openssl rand 30 | base64 -w 0` >> .env.prod
+echo POSTGRES_PASSWORD=`openssl rand 30 | base64 -w 0` >> .env.prod
  
 # Phase 1
-docker compose -f ./docker-compose-initiate.prod.yml up -d nginx
-docker compose -f ./docker-compose-initiate.prod.yml up certbot
-docker compose -f ./docker-compose-initiate.prod.yml down
+docker compose -f ./docker-compose-initiate.prod.yml up -d nginx --env-file .env.prod
+docker compose -f ./docker-compose-initiate.prod.yml up certbot --env-file .env.prod
+docker compose -f ./docker-compose-initiate.prod.yml down 
  
 # some configurations for let's encrypt
 #curl -L --create-dirs -o ./deployment/nginx/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf

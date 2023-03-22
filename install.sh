d7a72af9f2db60c3a11820e83f62fe0674bd66ad
Thomas Gudin <gudinthomas@gmail.com>
2023-03-22 16:54:31 +0100

    feat(env): update env file

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ EMAIL=$2
 
 # TODO : generate .env variables
 #echo MARIADB_USER=matomo > .env.prod
-echo POSTGRES_PASSWORD=`openssl rand 30 | base64 -w 0` >> .env.prod
+echo POSTGRES_PASSWORD=`openssl rand 30 | base64 -w 0` >> .env
  
 # Phase 1
-docker compose -f ./docker-compose-initiate.prod.yml up -d nginx --env-file .env.prod
-docker compose -f ./docker-compose-initiate.prod.yml up certbot --env-file .env.prod
+docker compose -f ./docker-compose-initiate.prod.yml up -d nginx
+docker compose -f ./docker-compose-initiate.prod.yml up certbot
 docker compose -f ./docker-compose-initiate.prod.yml down 
  
 # some configurations for let's encrypt

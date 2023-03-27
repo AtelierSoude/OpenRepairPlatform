a91964f8c69f0f9e0b56a9cd5dfdd105effe8059
Thomas Gudin <gudinthomas@gmail.com>
2023-03-27 12:09:31 +0200

    fix

--- deployment/django/start.sh
+++ deployment/django/start.sh
@@ -7,5 +7,5 @@ service cron start
 
 python3 ./manage.py migrate
 python3 ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
-python3 ./manage.py collectstatic --noinput
+#python3 ./manage.py collectstatic --noinput
 uwsgi uwsgi.ini

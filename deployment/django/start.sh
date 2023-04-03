b47883f85d8107b3d8aea70c79f238ebea933e17
Gudin Thomas <gudinthomas@gmail.com>
2023-04-03 18:34:35 +0200

    feat(npm): add modules compilation on start

--- deployment/django/start.sh
+++ deployment/django/start.sh
@@ -8,4 +8,8 @@ service cron start
 python3 ./manage.py migrate
 python3 ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
 #python3 ./manage.py collectstatic --noinput
+
+npm run buildprod -prefix ../../openrepairplatform/static/
+ 
+
 uwsgi uwsgi.ini

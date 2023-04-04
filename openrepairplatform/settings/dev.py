9ce81f50f5cb8601dfce69a5558bebf045902942
Gudin Thomas <gudinthomas@gmail.com>
2023-04-04 15:23:04 +0200

    fix(dev): fix media path

--- openrepairplatform/settings/dev.py
+++ openrepairplatform/settings/dev.py
@@ -1,5 +1,9 @@
 from .base import *  
 
+#pourquoi ? ... parceque...
+MEDIA_ROOT = "/srv/media/openrepairplatform_media/"
+
+
 INSTALLED_APPS += ["debug_toolbar","livereload"]  
 
 MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  

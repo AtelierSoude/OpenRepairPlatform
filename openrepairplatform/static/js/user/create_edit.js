379d1c6d22d247083b63146f2ec7d4f99ee91311
Gudin Thomas <gudinthomas@gmail.com>
2023-06-01 11:46:05 +0200

    refacto(bs-modal): update to bs-modal 3

--- openrepairplatform/static/js/user/create_edit.js
+++ openrepairplatform/static/js/user/create_edit.js
@@ -1,1 +1,12 @@
 let inputSelector = 'input[name="street_address"]';
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    var addButtons = document.getElementsByClassName("bs-gobal-modal");
+    for (var index=0; index < addButtons.length; index++) {
+  console.log();
+      modalForm(addButtons[index], {
+        formURL: addButtons[index]["dataset"]["formUrl"],
+        isDeleteForm: false
+      });
+    }
+  });

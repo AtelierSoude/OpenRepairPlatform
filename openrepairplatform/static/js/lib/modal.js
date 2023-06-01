1a07805c0257106deb54f058c2068274a26f1cc6
Gudin Thomas <gudinthomas@gmail.com>
2023-06-01 13:59:02 +0200

    fix

--- openrepairplatform/static/js/lib/modal.js
+++ openrepairplatform/static/js/lib/modal.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', (e) => {
     var addButtons = document.getElementsByClassName("bs-gobal-modal");
     for (var index=0; index < addButtons.length; index++) {

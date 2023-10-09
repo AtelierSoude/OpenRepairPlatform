ef7b06075daeea92181363eae0a06d386a84ffbc
Vescanarius <gudinthomas@gmail.com>
2023-10-09 12:25:32 +0200

    refacto(users): update modals for user page

--- openrepairplatform/static/js/lib/modal.js
+++ openrepairplatform/static/js/lib/modal.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     var addButtons = document.getElementsByClassName("bs-gobal-modal");
     for (var index=0; index < addButtons.length; index++) {
-  console.log("test");
       modalForm(addButtons[index], {
         formURL: addButtons[index]["dataset"]["formUrl"],
         isDeleteForm: false

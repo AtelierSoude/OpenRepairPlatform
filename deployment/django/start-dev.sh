25e06c9599273144ff488fbc5b7a83ff36997686
ClemsP <clement.poudret@laposte.net>
2023-11-28 20:02:15 +0100

    repair cron , change colors and do somes others things

--- deployment/django/start-dev.sh
+++ deployment/django/start-dev.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-#sed -i "s/{{SECRET_KEY}}/${SECRET_KEY}/" /etc/cron.d/openrepairplatform
-#sed -i "s/{{EMAIL_PASSWORD}}/${EMAIL_PASSWORD}/" /etc/cron.d/openrepairplatform
-#service cron start
-#chown -R openrepairplatform:openrepairplatform /srv/*
-python3 ../../manage.py migrate
-python3 ../../manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
+## ugly but this is the only way i found to make cron working 
+printenv > /etc/default/locale
+
+python3 manage.py migrate
+python3 manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
 #python3 ../../manage.py collectstatic --noinput
 #uwsgi ./uwsgi.ini
-
-npm run build -prefix ../../openrepairplatform/static/ &
+npm run build -prefix /srv/app/openrepairplatform/static/ &
 #python3 ../../manage.py runserver 0.0.0.0:80 --insecure &
-
+service cron start
 #python3 -m ptvsd --host 0.0.0.0 --port 5678 ../../manage.py runserver 0.0.0.0:80 --noreload --insecure &
-python3  ../../manage.py runserver 0.0.0.0:80 --noreload --insecure &
-python3 ../../manage.py livereload --host=0.0.0.0
+python3  manage.py runserver 0.0.0.0:80 --noreload --insecure &
+python3  manage.py livereload --host=0.0.0.0
+
 
 #ptvsd --host 0.0.0.0 --port 5678 &
 #

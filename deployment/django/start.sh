aac62a599d932c2f5c5e89c2f8ac0c2a7939aa46
ClemsP <clement.poudret@laposte.net>
2023-12-04 21:37:54 +0100

    try repair statics

--- deployment/django/start.sh
+++ deployment/django/start.sh
@@ -7,7 +7,5 @@ echo "cron started"
 
 python3 ./manage.py migrate
 python3 ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
-# python3 ./manage.py collectstatic --noinput --clear
-python3 manage.py assets build
 
 uwsgi uwsgi.ini

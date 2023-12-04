0d89b308eaff6bdc4edf8c78245ce10dabd58b77
ClemsP <clement.poudret@laposte.net>
2023-12-04 20:26:15 +0100

    try repair static

--- deployment/django/start.sh
+++ deployment/django/start.sh
@@ -10,6 +10,6 @@ python3 ./manage.py migrate
 python3 ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
 python3 ./manage.py collectstatic --noinput --clear
 
-npm run buildprod --unsafe-perm=true -prefix /srv/static/
+npm run buildprod -prefix /srv/static/
 
 uwsgi uwsgi.ini

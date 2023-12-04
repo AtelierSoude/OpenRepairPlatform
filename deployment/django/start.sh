597d4d8143ccc23dc36f6470ed036b19bee07477
ClemsP <clement.poudret@laposte.net>
2023-12-04 20:29:55 +0100

    try repair static

--- deployment/django/start.sh
+++ deployment/django/start.sh
@@ -5,10 +5,9 @@ echo "Start cron"
 cron
 echo "cron started"
 
-python3 ./manage.py assets build
 python3 ./manage.py migrate
 python3 ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
-python3 ./manage.py collectstatic --noinput --clear
+#python3 ./manage.py collectstatic --noinput --clear
 
 npm run buildprod -prefix /srv/static/
 

76b405ca54a4ba86337d90d04d2d9778e80821f5
JonasFW13 <jturbeaux@pm.me>
2025-06-11 15:27:35 +0200

    Change for dev pupose

--- deployment/django/start-dev.sh
+++ deployment/django/start-dev.sh
@@ -5,16 +5,15 @@ echo "Start cron"
 cron
 echo "cron started"
 
-python3 manage.py migrate
-python3 manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
+npm install -prefix /srv/static/
+npm run build -prefix /srv/static/
 
-npm install -prefix /srv/static/ &
-npm run build -prefix /srv/static/ &
+#python3 manage.py migrate
+#python3 manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
 
-python3  manage.py runserver 0.0.0.0:80 --noreload --insecure &
-python3  manage.py livereload --host=0.0.0.0
+#python3  manage.py runserver 0.0.0.0:8005
+#python3  manage.py livereload --host=0.0.0.0
 
 
 #ptvsd --host 0.0.0.0 --port 5678 &
-#
-uwsgi ./uwsgi.ini
+# uwsgi ./uwsgi.ini

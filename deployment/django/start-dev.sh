2ca80d599f6228eb40477c51c9cfbc811f4849fb
ClemsP <clement.poudret@laposte.net>
2025-07-25 12:54:00 +0200

    Update start-dev.sh

--- deployment/django/start-dev.sh
+++ deployment/django/start-dev.sh
@@ -11,12 +11,10 @@ npm run build -prefix /srv/static/ &
 python3 manage.py migrate
 python3 manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
 
-npm install -prefix /srv/static/ 
-npm run build -prefix /srv/static/ 
+python3  manage.py runserver 0.0.0.0:80 --noreload --insecure &
+python3  manage.py livereload --host=0.0.0.0
 
-#python3  manage.py runserver 0.0.0.0:80 --noreload --insecure &
-
-#python3  manage.py livereload --host=0.0.0.0
 
 #ptvsd --host 0.0.0.0 --port 5678 &
-# uwsgi ./uwsgi.ini
+#
+uwsgi ./uwsgi.ini

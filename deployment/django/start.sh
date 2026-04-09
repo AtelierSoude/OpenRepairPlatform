b29d905a6d12daa399a3b27b42ecd74c0fdd34d7
ClemsP <clement.poudret@laposte.net>
2026-04-09 16:36:58 +0200

    fix bad PATH location for user openrepairplatform

--- deployment/django/start.sh
+++ deployment/django/start.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-env > /etc/default/locale
+set -e
 
-# Ensure the installed binary is on the `PATH`
-ENV PATH="/root/.local/bin/:$PATH"
+env > /etc/default/locale
 
 echo "Start cron"
 cron
 echo "cron started"
 
-npm install -prefix /srv/static/
-npm run buildprod -prefix /srv/static/ &
+npm install --prefix /srv/static/
+npm run buildprod --prefix /srv/static/ &
 
 uv run ./manage.py migrate
 uv run ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true

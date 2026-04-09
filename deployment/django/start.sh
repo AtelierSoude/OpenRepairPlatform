df544c29d7ef489042add1e37ab3b9eea1ff3a41
ClemsP <clement.poudret@laposte.net>
2026-04-09 15:27:11 +0200

    add ommited uv run

--- deployment/django/start.sh
+++ deployment/django/start.sh
@@ -8,7 +8,7 @@ echo "cron started"
 npm install -prefix /srv/static/
 npm run buildprod -prefix /srv/static/ &
 
-python3 ./manage.py migrate
-python3 ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
+uv run ./manage.py migrate
+uv run ./manage.py shell -c "from openrepairplatform.user.models import CustomUser; CustomUser.objects.filter(email='admin@example.com').exists() or CustomUser.objects.create_superuser('admin@example.com', 'adminpass')" || true
 
 uwsgi uwsgi.ini
